ac43f7ca038512d4e442e153413efdd527af0a50
jryelsik <yelsikjustin@gmail.com>
2023-04-15 11:26:02 -0400

    Removed output dir; concat_list now removed after completion; Added red color if no media in dir; changed output file name

--- concat.sh
+++ concat.sh
@@ -5,12 +5,13 @@ GREEN=$(tput setaf 2)
 LIME_YELLOW=$(tput setaf 190)
 BRIGHT=$(tput bold)
 NORMAL=$(tput sgr0)
+RED=$(tput setaf 1)
 
 for dir in ${1%/}
 do
     if test $(find "$dir" -maxdepth 1 -type f -iname "*.MP4" | wc -c) -eq 0
     then        
-        printf "\nNo media files found in directory '"${dir##*/}"'\n"
+        printf "\n${RED}No MP4 files found in directory '"${dir##*/}"'\n${NORMAL}"
     else
 
     printf "\nFound media in directory '"${dir##*/}"'"
@@ -26,28 +27,22 @@ do
 
     printf "\nCreated 'concat_list.txt' in directory '"${dir##*/}"'\n"
 
-    # Add ouput directory
-    if [ ! -d "$dir"/output ]; then
-        printf "Added output directory to '"${dir##*/}"'\n"
-        mkdir "$dir"/output
-    else
-        printf "'output' directory already exists\n"
-    fi
-
-    printf "Concatenating listed media to output/"${dir##*/}".MP4'\n\n"
+    printf "Concatenating listed media to "${dir##*/}_OUT".MP4'\n\n"
 
     # Print found media sorted by date to console
     cat "$dir"/concat_list.txt ; echo
 
     # Start ffmpeg concatenation
-    ffmpeg -f concat -safe 0 -i "$dir"/concat_list.txt -c copy "$dir"/output/"${dir##*/}".MP4
+    ffmpeg -f concat -safe 0 -i "$dir"/concat_list.txt -c copy "$dir"/"${dir##*/}_OUT".MP4
 
-    #printf "Removed 'concat_list.txt' from directory '"${dir##*/}"'\n"
-    #rm "$dir"/concat_list.txt
+    printf "\nConcatenated below media files into ${dir##*/}_OUT.MP4\n" ; cat "$dir"/concat_list.txt ; echo
 
-    printf "\n${GREEN}${BRIGHT}Operation Completed Successfully\n${NORMAL}"  
+    printf "Removed 'concat_list.txt' from directory '"${dir##*/}"'\n"
+    rm "$dir"/concat_list.txt
 
-    printf "${LIME_YELLOW}*** Output file located in "$dir"/output/"${dir##*/}".MP4\n\n${NORMAL}"
+    printf "\n${GREEN}${BRIGHT}Operation Completed\n${NORMAL}"  
+
+    printf "${LIME_YELLOW}*** Output file located in "$dir"/"${dir##*/}_OUT".MP4\n\n${NORMAL}"
     fi
 done
 
